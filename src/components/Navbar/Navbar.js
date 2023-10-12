33f9a984736e0f990bea833bf5b196a0441071d8
Awadhesh Kumar Verma <vermaavd17476765@gmail.com>
2023-10-12 22:42:31 +0530

    yes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+// import { FaTimes } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import "./navbar.css";
 
 function Navbar() {
