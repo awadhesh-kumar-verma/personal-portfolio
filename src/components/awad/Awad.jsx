53a52efd16443eca78b53294b8d7dfff1c450f35
Awadhesh Kumar Verma <vermaavd17476765@gmail.com>
2023-10-26 20:08:52 +0530

    yes

--- src/components/awad/Awad.jsx
+++ src/components/awad/Awad.jsx
@@ -13,27 +13,28 @@ const Awad = () => {
             <div className='container header__container'>
                 <h5 className='hello' >Hi There, I'm</h5>
                 <p><span className='name' >Awadhesh Kumar</span> Verma</p>
+                
                 {/* <h5 className='texti text' >MERN Stack Developer || Competitive Programmer </h5> */}
                 {/* <EFFECT /> */}
 
                 <h5>
-                <TypeAnimation className='texti' 
-                    sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                        '➡️ MERN Stack Developer',
-                        1000, // wait 1s before replacing "Mice" with "Hamsters"
-                        '➡️ Competitive Programmer',
-                        1000,
-                        '➡️ UI/UX Developer',
-                        1000,
-                        '➡️ Open-Source Contributor',
-                        1000
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    style={{ fontSize: '1.3em', display: 'inline-block' }}
-                    repeat={Infinity}
-                />
+                    <TypeAnimation className='texti'
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            '➡️ MERN Stack Developer',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            '➡️ Competitive Programmer',
+                            1000,
+                            '➡️ UI/UX Developer',
+                            1000,
+                            '➡️ Open-Source Contributor',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{ fontSize: '1.3em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
                 </h5>
 
 
