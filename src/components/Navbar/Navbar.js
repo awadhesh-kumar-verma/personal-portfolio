4fef1dc51e1f24648d5b2367f869e2dddfd8f44f
Awadhesh Kumar Verma <vermaavd17476765@gmail.com>
2023-10-28 16:01:46 +0530

    yes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,12 @@
 import { useRef } from 'react';
-// import { FaTimes } from 'react-icons/fa';
 import { FaBars } from 'react-icons/fa';
 
-import {AiOutlineHome} from 'react-icons/ai';
-import {BiUser} from 'react-icons/bi';
-import {BiBook} from 'react-icons/bi';
-import {PiProjectorScreenChartLight} from 'react-icons/pi';
-import {LiaCertificateSolid} from 'react-icons/lia';
-import {BiMessageSquareDetail} from 'react-icons/bi';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
+import { BiBook } from 'react-icons/bi';
+import { PiProjectorScreenChartLight } from 'react-icons/pi';
+import { LiaCertificateSolid } from 'react-icons/lia';
+import { BiMessageSquareDetail } from 'react-icons/bi';
 
 import "./navbar.css";
 
@@ -30,22 +29,23 @@ function Navbar() {
                 <a href='#portfolio'><span className='deco' >Projects</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
                 <a href='#testimonials'>&nbsp;<span className='deco' >Certificates</span></a>
                 <a href='#contact'>&nbsp;&nbsp;<span className='deco' >Contact</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                {/* <button className='navi-btn navi-close-btn' onClick={showNavbar} >
-                    <FaTimes />
-                </button> */}
             </navi>
 
             <navi2 ref={navRef} onClick={showNavbar} >
 
-                <a href='#home'>< AiOutlineHome />&nbsp; Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                <a href='#about'>< BiUser />&nbsp; About&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                <a href='#experience'>< BiBook />&nbsp; Experience</a>
-                <a href='#portfolio'>< PiProjectorScreenChartLight /> &nbsp;Projects&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                <a href='#testimonials'>&nbsp;< LiaCertificateSolid />&nbsp; Certificates</a>
-                <a href='#contact'>&nbsp;&nbsp;< BiMessageSquareDetail />&nbsp; Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-                {/* <button className='navi-btn navi-close-btn' onClick={showNavbar} >
-                    <FaTimes />
-                </button> */}
+                <div>
+                    <div className='er1' >
+
+                    </div>
+                    <div className='er2' >
+                        <a href='#home'>< AiOutlineHome />&nbsp; Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                        <a href='#about'>< BiUser />&nbsp; About&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                        <a href='#experience'>< BiBook />&nbsp; Experience</a>
+                        <a href='#portfolio'>< PiProjectorScreenChartLight /> &nbsp;Projects&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                        <a href='#testimonials'>&nbsp;< LiaCertificateSolid />&nbsp; Certificates</a>
+                        <a href='#contact'>&nbsp;&nbsp;< BiMessageSquareDetail />&nbsp; Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    </div>
+                </div>
             </navi2>
             <button className='navi-btn' onClick={showNavbar} >
                 <FaBars />
