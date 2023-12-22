f2cc74de1267100c1bd583d88343a903c263a60f
Awadhesh Kumar Verma <vermaavd17476765@gmail.com>
2023-12-23 02:24:58 +0530

    yes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -187,6 +187,14 @@ const Experience = () => {
                                 <small className='text-light1' >Experienced</small>
                             </div>
                         </article>
+
+                        <article className='experience__details' >
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Git</h4>
+                                <small className='text-light1' >Experienced</small>
+                            </div>
+                        </article>
                     </div>
                 </div>
 
